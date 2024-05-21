dc7948e278cbb41b74a0125a36826dc1bcbbe1fc
10210796 <10210796@B207PC09>
2024-05-21 11:53:30 +0300

    Добавьте файлы проекта.

---
+++ DotaSHOP/Authorization.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DotaSHOP
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AppFrame.mainframe.Navigate(new Registration());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var userObj = AppConnect.model0db.users.FirstOrDefault(x => x.email == Email.Text && x.password == Password.Password);
+            if (userObj != null)
+            {
+                Window1 window = new Window1();
+                window.Show();
+                Application.Current.MainWindow.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль");
+            }
+        }
+    }
+}
